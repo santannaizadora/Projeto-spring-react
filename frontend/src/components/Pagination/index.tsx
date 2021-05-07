87f3f360085aa6c995c5c67370d0f7e30f8f0f66
Izadora Sant'Anna <santannaizadora@gmail.com>
2021-05-07 15:21:56 -0300

    DataTable, pagination

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -1,21 +1,28 @@
-function Pagination() {
+import { SalePage } from "types/sale";
+
+type Props = {
+    page: SalePage;
+    onPageChange: Function;
+}
+
+function Pagination({page, onPageChange}:Props) {
     return (
         <div className="row d-flex justify-content-center">
-        <nav>
-            <ul className="pagination">
-                <li className="page-item">
-                    <button className="page-link">Anterior</button>
-                </li>
-                <li className="page-item disabled">
-                    <span className="page-link">1</span>
-                </li>
-                <li className="page-item disabled">
-                    <button className="page-link">Próxima</button>
-                </li>
-            </ul>
-        </nav>
-    </div>
-    );
+            <nav>
+                <ul className="pagination">
+                    <li className={`page-item ${page.first ? 'disabled' : ''} `}>
+                        <button className="page-link" onClick={() => onPageChange(page.number - 1)}>Anterior</button>
+                    </li>
+                    <li className="page-item disabled">
+                        <span className="page-link">{page.number + 1}</span>
+                    </li>
+                    <li className={`page-item ${page.last ? 'disabled' : ''} `}>
+                        <button className="page-link" onClick={() => onPageChange(page.number + 1)}>Próxima</button>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    )
 }
 
 export default Pagination;
